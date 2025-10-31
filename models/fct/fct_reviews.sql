80404f91c011c2a02b2d08fed8a536e63184bf41
tony-soft-dev <campbellanthony04@gmail.com>
2025-10-31 14:58:24 -0600

    Adding documentation and packages

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -1,13 +1,16 @@
 {{
     config(
         materialized='incremental',
-        on_schema_change='fail',
+        on_schema_change='fail'
     )
 }}
 WITH src_reviews AS (
     SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT * FROM src_reviews
+SELECT 
+    {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+    * 
+FROM src_reviews
 WHERE review_text IS NOT NULL
 {% if is_incremental() %}
     AND review_date > (SELECT MAX(review_date) FROM {{ this }})
---
+++ models/overview.md
@@ -0,0 +1,9 @@
+{% docs __overview__ %}
+# Airbnb pipeline
+
+Hey, welcome to our Airbnb pipeline documentation!
+
+Here is the schema of our input data:
+![input schema](assets\IMG_4334.jpg)
+
+{% enddocs %}
